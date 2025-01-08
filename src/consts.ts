6957f81c7a3a08342323d5d60b265da1b97ec7c0
BHanleyPortfolio <95257871+BHanleyPortfolio@users.noreply.github.com>
2025-01-07 22:36:46 -0500

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Sekaicon';
+export const SITE_DESCRIPTION = 'Register for Sekaicon 2025 Today!April 11 - 13, 2025 Cherry Valley Hotel & Ohio Event Center, Newark OH Register Join us for Sekaicon, Central Ohio\'s newest anime and pop culture convention!Sekaicon 2025 is your chance to be among the very first to dive into a world of vibrant fandoms, incredible panels, and unforgettable experiences. Join us for a weekend […]';
